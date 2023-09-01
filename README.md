8990a7551ff128752d8a18f1453c8b1b5f990684
[Jason Cano] <jasoncano0509@gmail.com>
2023-09-01 10:34:34 -0500

    completed forecast function and stores search history locally.

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# weather-dashboard
+# Weather-Dashboard
+
+
+## Description
+A 5 day forecaster that will also give current weather values for your city search.
+
+![app_image](assets/images/mockuip.png)
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+- [Features](#features)
+- [Tests](#tests)
+- [Contact](#contact)
+
+## Installation
+For installation an openweahermap APIKey is required,
+
+## Usage
+Current ad forecasted weather over any 
+
+## Credits
+Jason Cano
+
+
+
+## Features
+5 day forecast, saves your search history, 
+
+## Tests
+When you search for a city it returns the current and forecasted weather.
+
+## Contact
+If there are any questions of concerns, I can be reached at:
+[github: jasoncano1](https://github.com/jasoncano1)
+##### [email: jasoncano0509@gmail.com] 
+##### (mailto:jasoncano0509@gmail.com)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,59 @@
+let store = [];
+
+const handleStorage = async () => {
+    store = await localStorage.cities ? JSON.parse(localStorage.cities) : [];
+
+    if (store.length) {
+        historyDiv.innerHTML = "";
+        store.forEach(city => {
+            historyDiv.innerHTML += `<button onclick="historyCity('${city}')">${city}</button>`;
+        });
+    };
+};
+
+const historyCity = async (city) => {
+    document.querySelector("input").value = city;
+    searchCity();
+};
+
+handleStorage();
 
 const searchCity = async () => {
     let city = document.querySelector("input").value;
     if (city) {
 
-    let url1 = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
-    let url2 = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`;
+        let url1 = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+        let url2 = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`;
 
-        current= await(await fetch(url1)).json();
-        forecast= await(await fetch(url2)).json();
+        let current = await (await fetch(url1)).json();
+        let { list: forecast } = await (await fetch(url2)).json();
+
+        if(!store.includes(current.name)){
+            store.push(current.name);
+            localStorage.setItem("cities", JSON.stringify(store));
+            handleStorage();
+        }
 
         currentDiv.innerHTML = `
-            <h2>${current.name} (${ new Date(current.dt).toLocaleDateString()}) <img src="https://openweathermap.org/img/w/${current.weather[0].icon}.png"</h2>
+            <h2>${current.name} (${new Date(current.dt).toLocaleDateString()}) <img src="https://openweathermap.org/img/w/${current.weather[0].icon}.png"</h2>
             <p>Temperature: ${current.main.temp} °F</p>
             <p>Humidity: ${current.main.humidity}%</p>
             <p>Wind Speed: ${current.wind.speed} MPH</p>
         `;
-        console.log (current, forecast);
-    
-}}
+
+        forecastDiv.innerHTML = "";
+        for (let i = 0; i < forecast.length; i = i + 8) {
+            forecastDiv.innerHTML += `
+                    <div class="card">
+                        <h3>${new Date(forecast[i].dt_txt).toLocaleDateString()}</h3>
+                        <img src="https://openweathermap.org/img/w/${forecast[i].weather[0].icon}.png">
+                        <p>Temp: ${forecast[i].main.temp} °F</p>
+                        <p>Humidity: ${forecast[i].main.humidity}%</p>
+                    </div>
+                `;
+        }
+    }
+}
 
 
 btn.onclick = searchCity;
